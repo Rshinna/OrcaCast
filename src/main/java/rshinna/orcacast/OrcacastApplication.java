dfff66cb082cad41867df41e094f0e61d9e336ff
Rodrigo Franco <rshina@hotmail.com>
2026-07-12 20:18:50 -0300

    chore: classe principal, resources e estrutura de testes

---
+++ src/main/java/rshinna/orcacast/OrcacastApplication.java
@@ -0,0 +1,13 @@
+package rshinna.orcacast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcacastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrcacastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rshinna/orcacast/OrcacastApplicationTests.java
@@ -0,0 +1,13 @@
+package rshinna.orcacast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcacastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
